b179c490bd0a7dfb59baf7916917be8e77d13934
Nachai <56039177+NaPryder@users.noreply.github.com>
2022-06-07 15:41:43 +0700

    Add Chapter 1

---
+++ Chapter 1/Ex1-Chapter 1 - ScriptMode.py
@@ -0,0 +1,20 @@
+
+print('Hello python')
+print("This is Script mode")
+
+print("""
+    This python script run from command prompt.
+
+    ---------------------
+End  .""")
+
+'Comment with single qoute'
+"Comment with double qoute"
+'''Comment with triple qoute'''
+"""Comment with triple qoute"""
+
+# print(' ** Thank you ** ')
+
+print('''** Good luck **''')
+
+print('ทดสอบการแสดงผล', "แบบหลายตัว")
